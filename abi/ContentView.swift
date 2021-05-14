3c6e4f949bd6e2ffe7d066f4d5b32c31e8ff4569
Estelle <66007150+meijerestelle@users.noreply.github.com>
2021-05-14 13:28:32 +0200

    Initial Commit

---
+++ abi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  abi
+//
+//  Created by Estèlle Meijer on 14/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ abi/abiApp.swift
@@ -0,0 +1,17 @@
+//
+//  abiApp.swift
+//  abi
+//
+//  Created by Estèlle Meijer on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct abiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
